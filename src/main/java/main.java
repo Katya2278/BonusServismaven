e9abaef821413a8e541445425e186f6d3d84bce7
katya <katerink11@yandex.ru>
2025-03-04 23:01:50 +0300

    correctid

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,17 +2,34 @@ public class main {
     public static void main(String[] args) {
         BonusServis service = new BonusServis();
 
-        int expected =150;
-        long amount =5_000;
+        int expected = 150;
+        long amount = 5_000;
         boolean registered = true;
         long actual = service.calculate(amount, registered);
         System.out.println("1. " + expected + " == ? ==" + actual);
 
         expected = 50;
         amount = 5_000;
-        registered=false;
+        registered = false;
         actual = service.calculate(amount, registered);
         System.out.println("2. " + expected + " == ? ==" + actual);
+
+        expected = 500;
+        amount = 70_000;
+        registered=false;
+        actual = service.calculate(amount, registered);
+        System.out.println("4. " + expected + " == ? ==" + actual);
+
+        expected = 500;
+        amount = 100_000;
+        registered = true;
+        actual = service.calculate(amount, registered);
+        System.out.println("3. " + expected + " == ? ==" + actual);
+
+
+
+
+
     }}
 
 
